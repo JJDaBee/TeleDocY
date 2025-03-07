1696a395a6490acf58209817b995c088f70802d6
AshleyTanyx <144514838+AshleyTanyx@users.noreply.github.com>
2025-03-07 15:27:59 +0800

    Modified consultationHistory.py

---
+++ deliveryDetail.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/deliveryDetail'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
+
+db = SQLAlchemy(app)
+
+class deliveryDetail(db.Model):
+    __tablename__ = "deliveryDetail"
+
+    deliveryAddress = db.Column(db.String(1000),primary_key=True, nullable=False)
+    medication = db.Column(db.String(1000), nullable=False)
+    deliverySurcharge = db.Column(db.Integer(1000), nullable=False)
+    deliveryDate = db.Column(db.DateTime,primary_key=True, nullable=False)
+
+@app.route("/order/deliveryDetail/<string:deliveryAddress>")
+
+def get_delivery_surcharge(deliveryAddress):
+    # Query the database to find the delivery details based on delivery address
+    delivery_detail = deliveryDetail.query.filter_by(deliveryAddress=deliveryAddress).first()
+    
+    # Check if the delivery address exists in the database
+    if not delivery_detail:
+        return jsonify({
+            "code": 404,
+            "message": "Delivery address not found."
+        }), 404
+    
+    # Return the delivery surcharge
+    return jsonify({
+        "code": 200,
+        "data": {
+            "deliverySurcharge": delivery_detail.deliverySurcharge,
+        }
+    }), 200
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
