1f1da2fdf3ba8a3d03dddedd374327001618a2d1
AshleyTanyx <144514838+AshleyTanyx@users.noreply.github.com>
2025-03-30 16:07:00 +0800

    edit

--- microservices/book_consult/book_consult.py
+++ microservices/book_consult/book_consult.py
@@ -4,10 +4,19 @@ import json
 import pika
 import sys, os
 from os import environ
+from pydantic import BaseModel
 
-import amqp_lib
+#import amqp_lib
 from invokes import invoke_http
 
 app = Flask(__name__)
+
+#URLs of existing microservices
+PATIENT_API_URL = "https://personal-gbst4bsa.outsystemscloud.com/PatientAPI/rest/patientAPI/patients"
+CONSULTATION_HISTORY_URL = "http://localhost:5000/consultation_history"
+SCHEDULE_URL= "http://localhost:5400/schedule"
+#DYTE_API_URL= some link
+NOTIFICATION_URL= "http://localhost:5300/notification"
+ORDER_SERVICES_URL = "http://localhost:5600/order_services"
 
 CORS(app)
