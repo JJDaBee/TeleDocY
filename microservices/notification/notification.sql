1c32e33f451d118bb825c0b2573bb1e373090b2a
JJDaBee <141636983+JJDaBee@users.noreply.github.com>
2025-04-06 15:32:47 +0800

    fixed order, consultationhistory, notification

--- microservices/notification/notification.sql
+++ microservices/notification/notification.sql
@@ -1,10 +1,16 @@
-CREATE TABLE IF NOT EXISTS notifications (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT,
+CREATE DATABASE IF NOT EXISTS `notification` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `notification`;
+
+DROP TABLE IF EXISTS `notifications`;
+
+CREATE TABLE IF NOT EXISTS `notifications` (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255),
     message TEXT,
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Optional: Insert sample data
 INSERT INTO notifications (email, message)
 SELECT 'vannessanga24@gmail.com', 'eat 1 panadol'
 WHERE NOT EXISTS (
--- microservices/order/order.sql
+++ microservices/order/order.sql
@@ -1,19 +1,20 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 
-CREATE DATABASE IF NOT EXISTS `order` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `order`;
+CREATE DATABASE IF NOT EXISTS `orderdb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `orderdb`;
 
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE IF NOT EXISTS `order` (
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE IF NOT EXISTS `orders` (
     `orderID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `uuid` CHAR(36) NOT NULL,
-    `medicines` JSON NOT NULL,
+    `prescriptions` JSON NOT NULL,
     `prescriptionDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Sample data for order logs --
-INSERT INTO `order` (`uuid`, `medicines`, `prescriptionDate`) VALUES
+INSERT INTO `orders` (`uuid`, `prescriptions`, `prescriptionDate`) VALUES
 (
     "uuid-1234",
     '[{"medicineName": "Panadol", "quantity": 2, "dosage": "2x daily"}, {"medicineName": "Vitamin C", "quantity": 1, "dosage": "1x daily"}]',
